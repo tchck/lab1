111a748e7d40b69f0336660465bb4ac729237ad1
Irina <isfn@list.ru>
2024-10-27 22:03:13 +0300

    11

---
+++ test/test/bird.cpp
@@ -0,0 +1,37 @@
+#include "bird.h"
+#include "animals.h"
+#include <string>
+Bird::Bird(std::string species, std::string color, std::string
+	food, std::string area)
+{
+	this->species = species;
+	this->color = color;
+	this->food = food;
+	this->area = area;
+	std::cout << "String default constructor called\n";
+}
+Bird::Bird(std::string parameters)
+{
+	this->species = parameters.substr(0, parameters.find(' '));
+	parameters.erase(0, parameters.find(' ') + 1);
+	this->color = parameters.substr(0, parameters.find(' '));
+	parameters.erase(0, parameters.find(' ') + 1);
+	this->food = parameters.substr(0, parameters.find(' '));
+	parameters.erase(0, parameters.find(' ') + 1);
+	this->area = parameters.substr(0, parameters.size());
+	std::cout << "Bird load-from-file constructor called\n";
+}
+Bird::~Bird()
+{
+	std::cout << "Bird default destructor called\n";
+}
+void Bird::print() {
+	std::cout << "Bird: " << "\n";
+	std::cout << species << " " << color << " " <<
+		food << " " << area << " " << "\n";
+}
+std::string Bird::getParam() {
+	std::string parameters;
+	parameters = "String " + species + " " + color + " " + food + " " + area;
+	return parameters;
+}
